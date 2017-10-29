32942137bd4ce4c0d9eea075228ada8eb7495d7f
sds0208 <sarahsantacruz86@gmail.com>
2017-10-29 23:13:40 +0000

    assignment in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029155651) do
+ActiveRecord::Schema.define(version: 20171029222959) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 20171029155651) do
     t.string "title"
     t.text "body"
     t.boolean "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
+RSpec.describe SponsoredPost, type: :model do
     let(:name) { RandomData.random_sentence }
     let(:description) { RandomData.random_paragraph }
     let(:title) { RandomData.random_sentence }
     let(:body) { RandomData.random_paragraph }
+    let(:price) { rand(200) }
     let(:topic) { Topic.create!(name: name, description: description) }
-    let(:post) { topic.posts.create!(title: title, body: body) }
+    let(:SponsoredPost) { topic.SponsoredPosts.create!(title: title, body: body, price: price) }
  
     it { is_expected.to belong_to(:topic) }
   
     describe "attributes" do
         it "has title and body attributes" do
-            expect(post).to have_attributes(title: title, body: body)
+            expect(SponsoredPost).to have_attributes(title: title, body: body, price: price)
         end
     end  
 end
