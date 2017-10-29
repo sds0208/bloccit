6f4144b5df2ceda99127d6c851ef94ce4031cd57
sds0208 <sarahsantacruz86@gmail.com>
2017-10-29 19:00:15 +0000

    checkpoint 21 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 require 'random_data'
 
+ # Create Topics
+ 15.times do
+   Topic.create!(
+     name:         RandomData.random_sentence,
+     description:  RandomData.random_paragraph
+   )
+ end
+ topics = Topic.all
+
 # Create Posts
 50.times do
     Post.create!(
+        topic:  topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
     )
@@ -27,6 +37,7 @@ end
 end    
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} advertisements created"
