01b436f7a658e66c514781ca01cf21e46074286f
sds0208 <sarahsantacruz86@gmail.com>
2017-11-10 00:41:39 +0000

    checkpoint 25 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :advertisements
   resources :topics do
-      resources :posts, except: [:index]
+  resources :posts, except: [:index]
+
   end
   resources :users, only: [:new, :create]
-
+  resources :sessions, only: [:new, :create, :destroy]
   get 'about' => 'welcome#about'
   
   get 'welcome/contact'
