f027aec8371058c08dae537e03d8c993b3b0de08
sds0208 <sarahsantacruz86@gmail.com>
2017-11-12 16:11:28 +0000

    checkpoint 27 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     before_save { self.email = email.downcase if email.present? }
     before_save :format_name
+    before_save { self.role ||= :member }
     
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6 }, if: Proc.new{|u| u.password_digest.nil?}
@@ -10,7 +11,9 @@ class User < ApplicationRecord
               presence: true,
               uniqueness: { case_sensitive: false },
               length: { minimum: 3, maximum: 254 }
-    has_secure_password   
+    has_secure_password
+    
+    enum role: [:member, :admin]
     
     def format_name
         self.name = name.split.map(&:capitalize).join(" ") if name
--- db/seeds.rb
+++ db/seeds.rb
@@ -47,10 +47,19 @@ end
     )
 end
 
-user = User.first
-user.update_attributes!(
-    email: 'sds02080208@gmail.com',
-    password: 'abc123'
+# Create an admin user
+admin = User.create!(
+    name:     'Admin User',
+    email:    'admin@example.com',
+    password: 'helloworld',
+    role:     'admin'
+)
+ 
+# Create a member
+member = User.create!(
+    name:     'Member User',
+    email:    'member@example.com',
+    password: 'helloworld'
 )
 
 puts "Seed finished"
