72ac4ccd7ce94ffce7c6254321cbc94503df42cb
sds0208 <sarahsantacruz86@gmail.com>
2017-11-13 02:07:47 +0000

    checkpoint 28 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  resources :advertisements
-  resources :topics do
-  resources :posts, except: [:index]
-
-  end
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
-  get 'about' => 'welcome#about'
-  
-  get 'welcome/contact'
-  
-  get 'welcome/faq'
-  
-  root 'welcome#index'
+    resources :advertisements
+    resources :topics do
+        resources :posts, except: [:index]
+    end
+    
+    resources :posts, only: [] do
+        resources :comments, only: [:create, :destroy]
+    end
+    
+    resources :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+    get 'about' => 'welcome#about'
+    
+    get 'welcome/contact'
+    
+    get 'welcome/faq'
+    
+    root 'welcome#index'
 
  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112151904) do
+ActiveRecord::Schema.define(version: 20171113010440) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20171112151904) do
     t.integer "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
